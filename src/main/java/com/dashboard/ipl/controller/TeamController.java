9ea58e0ed8a27577ea7e0ae33744d618ddcd8f83
deepa <deepravi0522@gmail.com>
2021-05-07 13:20:02 +0530

    first commit

---
+++ src/main/java/com/dashboard/ipl/controller/TeamController.java
@@ -0,0 +1,26 @@
+package com.dashboard.ipl.controller;
+
+import com.dashboard.ipl.model.Team;
+import com.dashboard.ipl.repository.MatchRepository;
+import com.dashboard.ipl.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/ipl-dashboard")
+public class TeamController {
+
+    @Autowired
+    TeamRepository teamRepository;
+
+    @Autowired
+    MatchRepository matchRepository;
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName){
+        Team team =  teamRepository.findByTeamName(teamName);
+        team.setMatches(matchRepository.getLatestMatchedByTeam(teamName,4));
+        return team;
+    }
+}
---
+++ src/main/java/com/dashboard/ipl/repository/TeamRepository.java
@@ -0,0 +1,8 @@
+package com.dashboard.ipl.repository;
+
+import com.dashboard.ipl.model.Team;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeamRepository extends CrudRepository<Team, Long> {
+    Team findByTeamName(String teamName);
+}
