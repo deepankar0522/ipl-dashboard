8769811c05a0890b0d1b3d48b482624a75914ae4
deepa <deepravi0522@gmail.com>
2021-05-10 23:40:56 +0530

    css update

--- src/main/dashboard-ui/src/components/MatchDetailCard.js
+++ src/main/dashboard-ui/src/components/MatchDetailCard.js
@@ -1,16 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./MatchSmallCard.scss";
 
 const MatchDetailCard = ({ teamName, latestMatch }) => {
-  console.log(latestMatch);
-  const otherTeam = teamName === latestMatch.team1 ? latestMatch.team2 : latestMatch.team1;
-  const otherTeamLink = "/teams/"+otherTeam;
+  // console.log(latestMatch);
+  const otherTeam =
+    teamName === latestMatch.team1 ? latestMatch.team2 : latestMatch.team1;
+  const otherTeamLink = "/teams/" + otherTeam;
   return (
     <div className="MatchDetailCard">
-      <h1>Latest Matches</h1>
-      <h2>vs <Link to= {otherTeamLink}>{otherTeam}</Link></h2>
-      <h4>at {latestMatch.venue}, {latestMatch.city}</h4>
-      <h3>{latestMatch.matchWinner} won by {latestMatch.resultMargin} {latestMatch.result}</h3>
+      <span className="vs">vs </span>
+      <h1>
+        <Link to={otherTeamLink}>{otherTeam}</Link>
+      </h1>
+      <h2>{latestMatch.date}</h2>
+      <h3>
+        at {latestMatch.venue}, {latestMatch.city}
+      </h3>
+      <h3>
+        {latestMatch.matchWinner} won by {latestMatch.resultMargin}{" "}
+        {latestMatch.result}
+      </h3>
     </div>
   );
 };
--- src/main/dashboard-ui/src/pages/TeamPage.js
+++ src/main/dashboard-ui/src/pages/TeamPage.js
@@ -4,12 +4,13 @@ import { useParams } from "react-router-dom";
 
 import MatchDetailCard from "../components/MatchDetailCard";
 import MatchSmallCard from "../components/MatchSmallCard";
+import "./TeamPage.scss";
 
 export const TeamPage = () => {
   const [teamData, setTeamData] = useState();
   const { teamName } = useParams();
   let URL = `http://localhost:8080/ipl-dashboard/teams/${teamName}`;
- // console.log(URL);
+  // console.log(URL);
   useEffect(() => {
     axios
       .get(URL)
@@ -18,14 +19,27 @@ export const TeamPage = () => {
       })
       .catch((e) => console.log("Error!, cannot fetch"));
   }, [URL]);
- // console.log(teamData);
+  // console.log(teamData);
   return (
     <div className="TeamPage">
-      <h1>{teamData && teamData.teamName}</h1>
-      {teamData && <MatchDetailCard latestMatch={teamData.matches[0]} teamName={teamName}/>}
+      <div className="team-name-section">
+        <h1 className="team-name">{teamData && teamData.teamName}</h1>
+      </div>
+      <div className="win-loss-section">wins /losses</div>
+      <div className="match-detail-section">
+      <h3>Latest Matches</h3>
+        {teamData && (
+          <MatchDetailCard
+            latestMatch={teamData.matches[0]}
+            teamName={teamName}
+          />
+        )}
+      </div>
       {teamData &&
         teamData.matches.slice(1).map((match) => {
-          return <MatchSmallCard key={match.id} match={match} teamName={teamName}/>;
+          return (
+            <MatchSmallCard key={match.id} match={match} teamName={teamName} />
+          );
         })}
     </div>
   );
